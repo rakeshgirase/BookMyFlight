208b3ffde8a777ef4b5b3334873d108dad663270
Rakesh Girase <girase.rakesh@gmail.com>
2017-09-06 21:32:21 +0100

    Removed Redundant Links

--- app/flights-app.component.ts
+++ app/flights-app.component.ts
@@ -3,7 +3,12 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'flights-app',
     template: `
-        <nav-bar></nav-bar>        
+        <nav-bar></nav-bar>
+        <h1>Angular Router</h1>
+        <nav>
+          <a routerLink="/flights" routerLinkActive="active">Flights</a>
+          <a routerLink="/flights/3" routerLinkActive="active">Flight Details</a>
+        </nav>
         <router-outlet></router-outlet>
         `
 })
